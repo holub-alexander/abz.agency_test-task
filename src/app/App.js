517986ca534de0fa1de5565ff636d726547f5221
Alexander Holub <holub.alexander.it@gmail.com>
2021-06-05 11:36:24 +0300

    refactor: apply prettier, apply stylelint, change structure of the project, add: icons, images, fonts, basic styles

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import image from '../assets/img/Banner_photo.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <img src={image} alt="" /> */}
+      <p>Lorem ipsum</p>
+    </div>
+  );
+}
+
+export default App;
